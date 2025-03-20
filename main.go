46192db37f20e064517ca98b635b89223e88cdde
Pacotacou <p4c0taco@gmail.com>
2025-03-20 16:39:59 -0500

    feat: clean code

--- main.go
+++ main.go
@@ -16,67 +16,94 @@ func main() {
 	}
 	switch command {
 	case "add":
-		if argsCount != 3 {
-			fmt.Println("Error: 1 arguments expected: 'description'")
-		}
-		taskList.AddTask(os.Args[2])
+		add(argsCount, &taskList)
 	case "update":
-		if argsCount != 4 {
-			fmt.Println("Error: 2 arguments expected: 'id', 'description'")
-		}
-		id, err := strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Println("Error: ID should be a number")
-		}
-		if err = taskList.UpdateTask(os.Args[3], id); err != nil {
-			fmt.Printf("Error: %v", err)
-		}
+		update(argsCount, &taskList)
 	case "delete":
-		if argsCount != 3 {
-			fmt.Println("Error: 1 argument expected: 'id'")
-		}
-		id, err := strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Println("Error: ID should be a number")
-		}
-		err = taskList.RemoveTask(id)
-		if err != nil {
-			fmt.Printf("Error, %v\n", err)
-		}
+		delete(argsCount, &taskList)
 	case "list":
-		if argsCount > 3 {
-			fmt.Println("Error: too many arguments")
-		} else if argsCount == 3 {
-			if err := taskList.ListTasks(os.Args[2]); err != nil {
-				fmt.Printf("Error: %v", err)
-			}
-		} else {
-			if err := taskList.ListTasks(""); err != nil {
-				fmt.Printf("Error: %v", err)
-			}
-		}
+		list(argsCount, &taskList)
 	case "mark-done":
-		if argsCount != 3 {
-			fmt.Println("Error 1 argument expeted: id")
-		}
-		id, err := strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Println("Error: id should be a number")
-		}
-		if err := taskList.MarkTask(models.StatusDone, id); err != nil {
-			fmt.Println("Error:", err)
-		}
+		markDone(argsCount, &taskList)
 	case "mark-in-progress":
-		if argsCount != 3 {
-			fmt.Println("Error 1 argument expeted: id")
-		}
-		id, err := strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Println("Error: id should be a number")
-		}
-		if err := taskList.MarkTask(models.StatusInProgress, id); err != nil {
-			fmt.Println("Error:", err)
-		}
+		markInProgress(argsCount, &taskList)
 	}
 
+}
+
+func markInProgress(argsCount int, taskList *models.TaskList) {
+	if argsCount != 3 {
+		fmt.Println("Error 1 argument expeted: id")
+	}
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Error: id should be a number")
+	}
+	if err := taskList.MarkTask(models.StatusInProgress, id); err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func markDone(argsCount int, taskList *models.TaskList) {
+	if argsCount != 3 {
+		fmt.Println("Error 1 argument expeted: id")
+	}
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Error: id should be a number")
+	}
+	if err := taskList.MarkTask(models.StatusDone, id); err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func list(argsCount int, taskList *models.TaskList) {
+	if argsCount > 3 {
+		fmt.Println("Error: too many arguments")
+	} else if argsCount == 3 {
+		if err := taskList.ListTasks(os.Args[2]); err != nil {
+			fmt.Printf("Error: %v", err)
+		}
+	} else {
+		if err := taskList.ListTasks(""); err != nil {
+			fmt.Printf("Error: %v", err)
+		}
+	}
+}
+
+func delete(argsCount int, taskList *models.TaskList) {
+	if argsCount != 3 {
+		fmt.Println("Error: 1 argument expected: 'id'")
+	}
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Error: ID should be a number")
+	}
+	err = taskList.RemoveTask(id)
+	if err != nil {
+		fmt.Printf("Error, %v\n", err)
+	}
+}
+
+func add(argsCount int, taskList *models.TaskList) {
+	if argsCount != 3 {
+		fmt.Println("Error: 1 arguments expected: 'description'")
+	}
+	err := taskList.AddTask(os.Args[2])
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func update(argsCount int, taskList *models.TaskList) {
+	if argsCount != 4 {
+		fmt.Println("Error: 2 arguments expected: 'id', 'description'")
+	}
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Error: ID should be a number")
+	}
+	if err = taskList.UpdateTask(os.Args[3], id); err != nil {
+		fmt.Printf("Error: %v", err)
+	}
 }
